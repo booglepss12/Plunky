fb3b3cd574e844d286c0b544b367ff4da6801767
Unknown <grooney4@gmail.com>
2018-05-10 20:40:29 -0400

    Level exit now functioning

--- Assets/LevelExit.cs
+++ Assets/LevelExit.cs
@@ -1,16 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class LevelExit : MonoBehaviour {
+public class LevelExit : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [SerializeField] float LevelLoadDelay = 2f;
+    [SerializeField] float LevelExitSlowMoFactor = 0.2f;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        StartCoroutine(LoadNextLevel());
+    }
+
+    IEnumerator LoadNextLevel()
+    {
+        Time.timeScale = LevelExitSlowMoFactor;
+        yield return new WaitForSecondsRealtime(LevelLoadDelay);
+        Time.timeScale = 1f;
+
+        var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
 }
